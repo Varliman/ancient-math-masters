32e68a87a55cd02061587a662117ac5478929880
Varliman <deniserileev85@gmail.com>
2026-04-30 01:39:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ancient-math-masters/",
   server: {
     host: "::",
     port: 8080,
